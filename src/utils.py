ed26f35a7ec949fca005220f1d404bf72b40f404
Irfan Malakkal <malakkalirfan@gmail.com>
2025-11-03 15:26:59 +0530

    flask implementation with some sort of error

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -19,18 +21,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
-        for model_name, model in models.items():
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
-            y_test_pred = model.predict(X_test)
-            train_model_score = r2_score(y_train, model.predict(X_train))
-            test_model_score = r2_score(y_test, y_test_pred)
-            report[model_name] = test_model_score
+
+            y_test_pred = model.predict(X_test)  # Predict on test data
+            test_model_score = r2_score(y_test, y_test_pred)  # Calculate R2 score
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
